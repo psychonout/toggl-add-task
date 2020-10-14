a79094c858213b4b4ee071ab751be817945e7542
max <max.o.harris@outlook.com>
2020-10-14 18:12:28 +0100

    add user ID to credentials and add priorities

--- add_task.py
+++ add_task.py
@@ -5,14 +5,24 @@ import requests
 from auth import auth_header
 from config import endpoints
 from datetime import datetime
+from config import toggl_creds
 
 
 def some_color():
     return "#%06x" % random.randint(0, 0xFFFFFF)
 
 
-def add_task(name, timeframe, date=datetime.now().date().isoformat()):
+def add_task(name, timeframe, priority, date=datetime.now().date().isoformat()):
     color = str(some_color().lower())
+    if (priority == 'hp'):
+        project_id = 1496530
+    elif (priority == 'i'):
+        project_id = 1496529
+    elif (priority == "sd"):
+        project_id = 1496914
+    else: 
+        # maintanence
+        project_id = 1496913
     params = {
         "color": color,
         "name": name,
@@ -20,15 +30,21 @@ def add_task(name, timeframe, date=datetime.now().date().isoformat()):
         "end_date": date,
         "estimated_minutes": timeframe,
         "status": "done",
-        "user_id": 6567265
+        "user_id": toggl_creds()["id"],
+        "project_id": project_id
     }
-    requests.post(endpoints()["tasks"],
+    result = requests.post(endpoints()["tasks"],
                   headers=auth_header(),
                   data=params)
+    print(result)
 
 
 if __name__ == "__main__":
     args = sys.argv[1:]
     name = args[0]
     timeframe = args[1]
-    add_task(name, timeframe)
+    try:
+        priority = args[2]
+    except Exception:
+        priority = "m"
+    add_task(name, timeframe, priority)
